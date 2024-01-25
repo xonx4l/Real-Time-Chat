0254b8456072b283afe181a87b3df8111198b9c7
xonx4l <sk797119@gmail.com>
2024-01-25 23:38:10 +0530

    create UserManager.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {server as WebSocketServer} from "websocket"
 import http from "http";
 
-var server = http.createServer(function(request:any, response:any) {
+const server = http.createServer(function(request:any, response:any) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
@@ -12,11 +12,6 @@ server.listen(8080, function() {
 
 const wsServer = new WebSocketServer({
     httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
     autoAcceptConnections: false
 });
 
@@ -36,6 +31,7 @@ wsServer.on('request', function(request) {
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
+       
         if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
             connection.sendUTF(message.utf8Data);
