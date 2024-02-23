914d2598ef7cf5b1d63f945f57ce93eb9a137b20
xonx4l <sk797119@gmail.com>
2024-02-23 23:38:00 +0530

    update UserManager.ts

--- src/UserManager.ts
+++ src/UserManager.ts
@@ -1,8 +1,27 @@
 
+interface User {
+    name: string;
+    id: string;
+}
+
+interface Room {
+    users:User[]
+}
+
 
 export class UserManager {
+    private users: Map<string , Room>;
     constructor() {
-        this.users = new Map<string, Room>
-        }
+        this.users = new Map<string, Room>()
+
     }
+
+    addUser(name: string, userId: string, roomId:string, socket: WebSocket) {
+
+    }
+
+    removeUser(roomId: string, userId:string) {
+        
+    }
+    } 
 
